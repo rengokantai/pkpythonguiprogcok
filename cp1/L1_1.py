115997af4ee2709a933be0c481a4b57417125f07
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-20 16:23:50 -0500

    combobox entry

--- cp1/L1_1.py
+++ cp1/L1_1.py
@@ -10,12 +10,18 @@ label.grid(column=0,row=0)
 textLabel = tk.StringVar()
 name = ttk.Entry(win,width=12, textvariable=textLabel)
 name.grid(column=0,row=2)
+name.focus()
 
 def clickMe():
     action.configure(text="clicked")
     label.configure(text=name.get())
+    action.configure(state="disabled")
 
-
+number = tk.StringVar()
+combobox = ttk.Combobox(win,width=12, textvariable=number,state='readonly')
+combobox['values']=(1,2,3,4)
+combobox.grid(column=0,row=4)
+combobox.current(0)
 
 
 action = ttk.Button(win, text="click",command=clickMe)
